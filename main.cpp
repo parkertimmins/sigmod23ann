2ae9c0515da282cedc32729123014d19653169a0
parkertimmins <parkertimmins@gmail.com>
2023-03-07 09:17:56 +0100

    use lhs hash funcions stored in unordered map

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include <algorithm>
 #include <queue>
 #include <random>
+#include <unordered_map>
+#include <cstdint>
 #include "io.h"
 
 
@@ -15,60 +17,95 @@ using std::endl;
 using std::string;
 using std::vector;
 
-
-
+using Vec = vector<float>;
 
 #define _INT_MAX 2147483640
 
-float EuclideanDistance(const vector<float> &lhs, const vector<float> &rhs) {
-  float ans = 0.0;
-  unsigned lensDim = 100;
-  for (unsigned i = 0; i < lensDim; ++i) {
-    ans += (lhs[i] - rhs[i]) * (lhs[i] - rhs[i]);
-  }
-  return ans;
-}
+/**
+ * Docs used
+ * https://www.pinecone.io/learn/locality-sensitive-hashing-random-projection/
+ * https://en.wikipedia.org/wiki/Locality-sensitive_hashing
+ */
 
-vector<uint32_t> CalculateOneKnn(const vector<vector<float>> &data,
-                                 const vector<uint32_t> &sample_indexes,
-                                 const uint32_t id) {
-  std::priority_queue<std::pair<float, uint32_t>> top_candidates;
-  float lower_bound = _INT_MAX;
-  for (unsigned i = 0; i < sample_indexes.size(); i++) {
-    uint32_t sample_id = sample_indexes[i];
-    if (id == sample_id) continue;  // skip itself.
-    float dist = EuclideanDistance(data[id], data[sample_id]);
-
-    // only keep the top 100
-    if (top_candidates.size() < 100 || dist < lower_bound) {
-      top_candidates.push(std::make_pair(dist, i));
-      if (top_candidates.size() > 100) {
-        top_candidates.pop();
-      }
-
-      lower_bound = top_candidates.top().first;
+float distance(const Vec &lhs, const Vec &rhs) {
+    float ans = 0.0;
+    unsigned lensDim = 100;
+    for (unsigned i = 0; i < lensDim; ++i) {
+        auto d = (lhs[i] - rhs[i]);
+        ans += (d * d);
     }
-  }
-
-  vector<uint32_t> knn;
-  while (!top_candidates.empty()) {
-    knn.emplace_back(top_candidates.top().second);
-    top_candidates.pop();
-  }
-  std::reverse(knn.begin(), knn.end());
-  return knn;
+    return ans;
 }
 
-void ConstructKnng(const vector<vector<float>> &data,
-                   const vector<uint32_t> &sample_indexes,
-                   vector<vector<uint32_t>> &knng) {
-  knng.resize(data.size());
-#pragma omp parallel for
-  for (uint32_t n = 0; n < knng.size(); n++) {
-    knng[n] = CalculateOneKnn(data, sample_indexes, n);
-  }
-  cout << "Construction Done!" << endl;
+std::default_random_engine rd(123);
+vector<float> makeRandoms(size_t len=100) {
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dist(-1.0, 1.0);
+
+    Vec randVec;
+    randVec.reserve(len);
+    while (randVec.size() < len) {
+        randVec.emplace_back(dist(gen));
+    }
+    return randVec;
 }
+
+float dot(const Vec& lhs, const Vec& rhs) {
+    float ans = 0.0;
+    for (unsigned i = 0; i < 100; ++i) {
+        ans += (lhs[i] * rhs[i]);
+    }
+    return ans;
+}
+
+uint64_t projectRandoms(const vector<Vec>& randVecs, const Vec& values) {
+    uint64_t projection = 0;
+    for (auto& rv : randVecs) {
+        auto dotProduct = dot(values, rv);
+        projection <<= 1;
+        projection |= (dotProduct > 0);
+    }
+    return projection; 
+}
+
+
+int numGroupBits = 10;
+void constructResult(const vector<Vec>& data, vector<vector<uint32_t>>& result) {
+ 
+    vector<Vec> randVecs;
+    while (randVecs.size() < numGroupBits) {
+        auto randv = makeRandoms();
+        randVecs.push_back(randv);
+    }
+    
+    std::unordered_map<uint64_t, vector<uint32_t>> groups;
+
+    // load vectors in map
+    for (uint32_t i = 0, len=data.size(); i < len; ++i) {
+        if (i % 100'000 == 0) std::cout << "inserted: " << i << std::endl;
+
+        uint64_t signature = projectRandoms(randVecs, data[i]);
+        if (groups.find(signature) == groups.end()) {
+            groups[signature] = { i };
+        } else {
+            groups[signature].push_back(i);
+        }
+    }
+    std::cout << groups.size() << std::endl;
+
+    result.resize(data.size());
+
+
+    for (uint32_t i = 0, len=data.size(); i < len; ++i) {
+        const auto& v = data[i];
+        uint64_t signature = projectRandoms(randVecs, v);
+
+        auto& group = groups[signature];
+        std::cout << group.size() << std::endl;
+    }
+
+}
+
 
 int main(int argc, char **argv) {
   string source_path = "../dummy-data.bin";
@@ -82,17 +119,10 @@ int main(int argc, char **argv) {
   vector<vector<float>> nodes;
   ReadBin(source_path, nodes);
 
-  // Sample 10,000 points for greedy search
-  std::default_random_engine rd;
-  std::mt19937 gen(rd());  // Mersenne twister MT19937
-  vector<uint32_t> sample_indexes(nodes.size());
-  iota(sample_indexes.begin(), sample_indexes.end(), 0);
-  shuffle(sample_indexes.begin(), sample_indexes.end(), gen);
-  sample_indexes.resize(10000);
 
   // Knng constuction
   vector<vector<uint32_t>> knng;
-  ConstructKnng(nodes, sample_indexes, knng);
+  constructResult(nodes, knng);
 
   // Save to ouput.bin
   SaveKNNG(knng);
