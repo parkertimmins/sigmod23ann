55a6a02e0a67c7180832fe47c346a1b3913955e4
parkertimmins <parkertimmins@gmail.com>
2023-03-27 22:10:18 +0200

    update run.sh to use CMakeLists.txt to link tbb correctly

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-make clean
-make
-./knng
-
-
-#mkdir -p build
-#cd build
-#cmake ..
 #make clean
 #make
-#cd ..
-#./build/sigmod23ann
+#./knng
+
+
+mkdir -p build
+cd build
+cmake ..
+make clean
+make
+cd ..
+./build/sigmod23ann
