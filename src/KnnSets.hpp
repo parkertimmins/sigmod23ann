5b73e57be515743f4423d575a5ed173f189c4099
parkertimmins <parkertimmins@gmail.com>
2023-04-06 12:10:50 +0200

    Fix bug in pointsCopy impl

--- src/KnnSets.hpp
+++ src/KnnSets.hpp
@@ -288,15 +288,15 @@ void addCandidatesCopy(
                    Range range,
                    vector<TKnnSet>& idToKnn) {
 
-    for (uint32_t i=range.first; i < range.second-1; ++i) {
-        std::memcpy(pointsCopy[i], points[indices[i]], 100 * sizeof(float));
+    for (uint32_t i=range.first; i < range.second; ++i) {
+        std::memcpy(pointsCopy[i], points[indices[i]], 112 * sizeof(float));
     }
     for (uint32_t i=range.first; i < range.second-1; ++i) {
         auto id1 = indices[i];
         auto& knn1 = idToKnn[id1];
         for (uint32_t j=i+1; j < range.second; ++j) {
             auto id2 = indices[j];
-            float dist = distance(pointsCopy[i], points[j]);
+            float dist = distance(pointsCopy[i], pointsCopy[j]);
             knn1.addCandidate(id2, dist);
             idToKnn[id2].addCandidate(id1, dist);
         }
